a61d72ed357787843d7a62057e9d0a2a644590ae
Antoniome89 <ajme89@gmail.com>
2019-03-18 12:29:56 +0100

    Primer Commit

---
+++ EjercicioNumeros.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicionumeros;
+
+/**
+ *
+ * @author tony_
+ */
+public class EjercicioNumeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Mostrar números del 100 al 1 utilizando bucle for;
+        
+        /*int limit = 0;
+        for (int counter = 100; counter > limit; counter --) {
+            System.out.println(counter);
+        }*/
+        
+        //Mostrar números del 100 al 1 utilizando bucle while;
+        
+        /*int counter = 100;
+        int limit = 0;
+        
+        while (counter > limit) {
+         System.out.println(counter);
+         counter --;*/
+           
+        
+        //Mostrar números del 100 al 1 utilizando bucle do while;
+        
+        int counter = 100;
+        int limit = 0;
+        
+        do {System.out.println(counter);
+            counter --;
+        
+        } while (counter > limit);
+        
+        
+        
+        }
+        }
+        
+        
+    
+    
+
